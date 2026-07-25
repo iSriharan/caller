5f4499588efee369bf1ac6685258b1ca1751849b
iSriharan <97461065+iSriharan@users.noreply.github.com>
2026-07-25 13:06:55 +0530

    Dialer
    Made UI responsive as user touches the number it will display in screen

--- lib/Dialer.dart
+++ lib/Dialer.dart
@@ -22,15 +22,38 @@ class _DialerPageState extends State<DialerPage> {
     '0',
     '*'
   ];
+
+  String txtfield = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: nums(),
+      body: _body(),
       backgroundColor: Colors.black,
     );
   }
 
-  Widget nums() {
+  Widget _body() {
+    return Column(
+      children: [
+        _display(),
+        numpad(),
+      ],
+    );
+  }
+
+  Widget _display() {
+    return Container(
+        height: 180,
+        color: Colors.black,
+        alignment: Alignment.center,
+        child: Text(
+          txtfield.isEmpty ? 'Enter' : txtfield,
+          style: TextStyle(fontSize: 40, letterSpacing: 3),
+        ));
+  }
+
+  Widget numpad() {
     // List<Widget> storage = [];
     // for (int i = 0; i < numbers.length; i++) {
     //   String n = numbers[i];
@@ -60,23 +83,30 @@ class _DialerPageState extends State<DialerPage> {
   }
 
   Widget _circle(String n) {
-    return SizedBox(
-      width: 80,
-      height: 80,
-      child: Container(
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.grey.shade800,
-          border: Border.all(
-            color: Colors.grey,
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          txtfield += n;
+        });
+      },
+      child: SizedBox(
+        width: 80,
+        height: 80,
+        child: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.grey.shade800,
+            border: Border.all(
+              color: Colors.grey,
+            ),
           ),
-        ),
-        child: Text(
-          n,
-          style: const TextStyle(
-            fontSize: 25,
-            color: Colors.white,
+          child: Text(
+            n,
+            style: const TextStyle(
+              fontSize: 25,
+              color: Colors.white,
+            ),
           ),
         ),
       ),
